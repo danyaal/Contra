ff186d111c07a1b24d8b8fac98562059ba662a29
unknown <deboltj@umich.edu>
2014-09-16 19:43:11 -0400

    lololol

---
+++ Assets/Floor.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Floor : MonoBehaviour {
+
+	public bool collidablep1;
+	public bool collidablep2;
+
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,129 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+	public Vector3		Velocity = Vector3.zero;
+	public float		Gravity	=	9.81f;
+	public float 		walkSpeed= 30f;
+
+	//flags all over this bitch
+
+	public bool			leftFlag=false;
+	public bool			rightFlag=false;
+	public bool 		upFlag=false;
+	public bool			downFlag = false;
+
+	public bool 		floating = false;
+	public bool			spindrive=false;
+	public bool			airLeft = false;
+	public bool 		airRight=false;
+
+
+	// Use this for initialization
+	void Start () {
+		floating = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown ("left")) {
+			leftFlag=true;
+			airLeft=true;
+			if(!rightFlag)
+			{airRight=false;}
+		}
+		if (Input.GetKeyDown("right")){
+			rightFlag=true;
+			airRight=true;
+			if(!leftFlag)
+			{airLeft=false;}
+		}
+		if (Input.GetKeyDown("up")){
+			upFlag=true;
+		}
+		if (Input.GetKeyDown("down")){
+			downFlag=true;
+		}
+
+		if (Input.GetKeyDown("a")&& !floating){
+			spindrive=true;
+		}
+
+		if (Input.GetKeyUp ("left")) {
+			leftFlag=false;
+			if(airRight|| !floating){
+				airLeft=false;
+			}
+		}
+		if (Input.GetKeyUp("right")){
+			rightFlag=false;
+			if(leftFlag|| !floating)
+			{airRight=false;}
+		}
+		if (Input.GetKeyUp("up")){
+			upFlag=false;
+		}
+		if (Input.GetKeyUp("down")){
+			downFlag=false;
+		}
+
+
+	}
+	void FixedUpdate()
+	{
+		Vector3 pos = transform.position;
+		if (spindrive) {
+			Velocity.y=10f;
+			floating=true;
+			spindrive=false;
+		}
+		if (floating) {
+			Velocity.y-=Gravity*2*Time.deltaTime;
+			if(airRight){
+				Velocity.x=walkSpeed*10;
+			}
+			else if(airLeft){
+				Velocity.x=walkSpeed*-10;
+			}
+		} 
+		else {
+			if (rightFlag)
+			{
+				Velocity.x=walkSpeed*10;
+			}
+			else if(leftFlag)
+			{
+				Velocity.x=walkSpeed*-10;
+			}
+			else
+			{
+				Velocity.x=0;
+			}
+		}
+		pos += Velocity * Time.deltaTime;
+		transform.position = pos;
+	}
+	void OnTriggerEnter(Collider col)
+	{
+		floating = false;
+		airLeft = false;
+		airRight = false;
+		Velocity.y = 0;
+		Vector3 pos = transform.position;
+		pos.y = col.bounds.max.y + transform.localScale.y+0.5f;
+		transform.position = pos;
+	}
+
+	void OnTriggerExit(Collider col)
+	{
+		floating = true;
+		airLeft = leftFlag;
+		airRight = rightFlag;
+	}
+	void OnTriggerStay(Collider col)
+	{
+
+	}
+
+	
+}
