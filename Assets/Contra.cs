44c862ef3d1817f07c059d457f10433daab3a6ce
Danyaal Rangwala <thedanyaal@gmail.com>
2014-09-16 20:25:52 -0400

    Materials for ground + character and other stuff

--- Assets/Contra.cs
+++ Assets/Contra.cs
@@ -3,9 +3,16 @@ using System.Collections;
 
 public class Contra : MonoBehaviour {
 
+	public GameObject GroundPrefab;
+
 	// Use this for initialization
 	void Start () {
-	
+		GameObject ground = Instantiate (GroundPrefab) as GameObject;
+		Vector3 pos = Vector3.zero;
+		pos.x = 16.67159f;
+		pos.y = -2.252744f;
+		pos.z = 0f;
+		ground.transform.position = pos;
 	}
 	
 	// Update is called once per frame
